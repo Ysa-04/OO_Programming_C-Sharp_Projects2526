91bfe7af347ad5b46a539055526c5c387fd8d2bb
Ysa-04 <ysaura2903@outlook.com>
2026-02-09 23:42:21 +0100

    Matthias gave me a cool method i can implement somewhere

--- Library/Program.cs
+++ Library/Program.cs
@@ -78,5 +78,73 @@
             Console.ReadLine();
             Console.ResetColor();
         }
+
+        
+        public static void ClearCurrentConsoleLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        // Question Y/N
+        private bool firstError = true;
+        public bool QuestionYN(string question, bool errorDisplay = true)
+        {
+            Console.Write($"{question} [Y/N] : ");
+            string input = Console.ReadLine();
+            bool returnValue = false;
+            try
+            {
+                if (input.ToUpper() == "Y")
+                {
+                    returnValue = true;
+                    RemoveErrorMessage(question + " [Y/N]", "Yes");
+                }
+                else if (input.ToUpper() == "N")
+                {
+                    returnValue = false;
+                    RemoveErrorMessage(question + " [Y/N]", "No");
+                }
+                else
+                {
+                    returnValue = QuestionYN(question, errorDisplay);
+                }
+            }
+            catch
+            {
+                if (errorDisplay)
+                {
+                    ErrorMessage(question);
+                }
+                returnValue = QuestionYN(question, errorDisplay);
+            }
+            firstError = true;
+            return returnValue;
+        }
+
+        private void ErrorMessage(string errorMessage = "")
+        {
+            ClearCurrentConsoleLine();
+            if (firstError == true)
+            {
+                ConsoleColor resetColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Input Invalid");
+                Console.ForegroundColor = resetColor;
+            }
+            firstError = false;
+        }
+        private void RemoveErrorMessage(string question, string value)
+        {
+            ClearCurrentConsoleLine();
+            if (!firstError)
+            {
+                ClearCurrentConsoleLine();
+            }
+            Console.WriteLine($"{question} : {value}");
+        }
     }
 }
