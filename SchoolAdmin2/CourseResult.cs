8ca7b4f9dd5f3d18154a9c8d70c3265d56bc228b
Ysa-04 <ysaura2903@outlook.com>
2026-08-20 11:49:49 +0200

    H10_8 SchoolAdmin properties CourseResult

--- SchoolAdmin2/CourseResult.cs
+++ SchoolAdmin2/CourseResult.cs
@@ -6,7 +6,31 @@ namespace SchoolAdmin2
 {
     internal class CourseResult
     {
-        public string Name;
-        public byte Result;
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int result;
+
+        public int Result
+        {
+            get { return result; }
+            set 
+            { 
+                if(value > 20)
+                {
+                    Console.WriteLine("Foutief cijfer!");
+                }
+                else
+                {
+                    result = value;
+                }
+            }
+        }
+
     }
 }
