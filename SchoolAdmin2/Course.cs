2a4932a8e6b36c796607660d44788a0f9ce22125
Ysa-04 <ysaura2903@outlook.com>
2026-08-20 10:32:23 +0200

    H10_4 SchoolAdmin

---
+++ SchoolAdmin2/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolAdmin2
+{
+    internal class Course
+    {
+        public string Title;
+        public List<Student> Students = new List<Student>();
+
+        public void ShowOverview()
+        {
+            Console.WriteLine($"{this.Title}:");
+            foreach (Student student in Students)
+            {
+                Console.WriteLine($"\t- {student.Name}");
+            }
+        }
+
+    }
+}
--- SchoolAdmin2/Program.cs
+++ SchoolAdmin2/Program.cs
@@ -10,6 +10,7 @@
         {
             Console.WriteLine("Wat wil je doen?");
             Console.WriteLine("1. DemonstreerStudenten uitvoeren");
+            Console.WriteLine("2. DemonstreerCursussen uitvoeren");
             Console.WriteLine();
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -17,6 +18,10 @@
                 case 1:
                     Console.WriteLine();
                     DemoStudents();
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    DemoCourses();
                     break;
                 default:
                     break;
@@ -34,7 +39,8 @@
             said.StudentNumber = Student.StudentCounter;
             Student.StudentCounter++;
             said.RegisterForCourse("Programmeren");
-            said.RegisterForCourse("Databanken");
+            said.RegisterForCourse("Webtechnologie");
+            said.RegisterForCourse("Communicatie");
 
             Student mieke = new Student();
             mieke.Name = "Mieke Vermeulen";
@@ -42,11 +48,57 @@
             mieke.StudentNumber = Student.StudentCounter;
             Student.StudentCounter++;
             mieke.RegisterForCourse("Communicatie");
+            mieke.RegisterForCourse("Programmeren");
+            mieke.RegisterForCourse("Databanken");
 
             Console.WriteLine(said.GenerateNamecard());
             Console.WriteLine(said.DetermineWorkload());
             Console.WriteLine(mieke.GenerateNamecard());
             Console.WriteLine(mieke.DetermineWorkload());
+        }
+
+        public static void DemoCourses()
+        {
+            Student said = new Student();
+            said.Name = "Said Aziz";
+            said.BirthDay = new DateTime(2000, 6, 1);
+            said.StudentNumber = Student.StudentCounter;
+            Student.StudentCounter++;
+            said.RegisterForCourse("Programmeren");
+            said.RegisterForCourse("Webtechnologie");
+            said.RegisterForCourse("Communicatie");
+
+            Student mieke = new Student();
+            mieke.Name = "Mieke Vermeulen";
+            mieke.BirthDay = new DateTime(1998, 1, 1);
+            mieke.StudentNumber = Student.StudentCounter;
+            Student.StudentCounter++;
+            mieke.RegisterForCourse("Communicatie");
+            mieke.RegisterForCourse("Programmeren");
+            mieke.RegisterForCourse("Databanken");
+
+            Course programmeren = new Course();
+            programmeren.Title = "Programmeren";
+            programmeren.Students.Add(said);
+            programmeren.Students.Add(mieke);
+
+            Course databanken = new Course();
+            databanken.Title = "Databanken";
+            databanken.Students.Add(mieke);
+
+            Course communicatie = new Course();
+            communicatie.Title = "Communicatie";
+            communicatie.Students.Add(said);
+            communicatie.Students.Add(mieke);
+
+            Course webtechnologie = new Course();
+            webtechnologie.Title = "Webtechnologie";
+            webtechnologie.Students.Add(said);
+
+            programmeren.ShowOverview();
+            databanken.ShowOverview();
+            communicatie.ShowOverview();
+            webtechnologie.ShowOverview();
         }
     }
 }
