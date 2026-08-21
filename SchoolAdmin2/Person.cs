3d69a31298f46a44adfd630a7a4ff69393293813
Ysa-04 <ysaura2903@outlook.com>
2026-08-21 10:53:15 +0200

    H14_1 SchoolAdmin: Persoon

---
+++ SchoolAdmin2/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace SchoolAdmin2
+{
+    internal class Person
+    {
+        private uint id;
+        public uint Id
+        {
+            get { return id; }
+        }
+
+        private static uint maxId = 1;
+
+        private DateTime birthDay;
+
+        public DateTime BirthDay
+        {
+            get { return birthDay; }
+        }
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.Now;
+                int years = now.Year - this.birthDay.Year;
+                if (now.Month < this.BirthDay.Month || now.Month == this.BirthDay.Month && now.Day == this.BirthDay.Day)
+                {
+                    years--;
+                }
+                return years;
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private static List<Person> allPersons = new List<Person>();
+        public static ImmutableList<Person> AllPersons
+        {
+            get { return allPersons.ToImmutableList<Person>();  }
+        }
+
+        public Person(string name, DateTime birthDay)
+        {
+            this.id = maxId;
+            maxId++;
+            this.Name = name;
+            this.birthDay = birthDay;
+            allPersons.Add(this);
+        }
+        public abstract double DetermineWorkload();
+        public abstract string GenerateNameCard();
+
+    }
+}
