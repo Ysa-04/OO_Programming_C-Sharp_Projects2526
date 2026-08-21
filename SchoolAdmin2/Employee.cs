fb6e334159d3eff72a47dd1a6ee843f81cc0ee80
Ysa-04 <ysaura2903@outlook.com>
2026-08-21 11:35:04 +0200

    H14_3 SchoolAdmin: Personeel erft over van Persoon

---
+++ SchoolAdmin2/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace SchoolAdmin2
+{
+    internal abstract class Employee:Person
+    {
+		private byte seniority;
+
+		public byte Seniority
+		{
+			get { return seniority; }
+			set 
+			{ 
+				if (value > 50)
+				{
+					seniority = 50;
+				}
+				else
+				{
+                    seniority = value;
+                }
+			}
+		}
+
+		private Dictionary<string, byte> tasks = new Dictionary<string, byte>();
+		public ImmutableDictionary<string,byte> Tasks
+		{
+			get
+			{
+				return tasks.ToImmutableDictionary<string, byte>();
+			}
+		}
+
+		private static List<Employee> allEmployees = new List<Employee>();
+		public static ImmutableList<Employee> AllEmployees
+		{
+			get { return allEmployees.ToImmutableList<Employee>(); }
+		}
+
+		public Employee(string name, DateTime birthDay, Dictionary<string,byte> tasks) : base(name, birthDay)
+		{
+			if(tasks is not null)
+			{
+				foreach(var item in tasks)
+				{
+					this.tasks.Add(item.Key, item.Value);
+				}
+			}
+			allEmployees.Add(this);
+		}
+
+		public abstract uint CalculateSalary();
+
+	}
+}
